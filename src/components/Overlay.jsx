8c022b0b7401666d4560727ed907577bc58c4447
hanan <asyrawih@gmail.com>
2024-09-19 23:09:34 +0700

    initialize
    initialze

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,36 @@
+import { useProgress } from "@react-three/drei";
+import { usePlay } from "../contexts/Play";
+
+export const Overlay = () => {
+  const { progress } = useProgress();
+  const { play, end, setPlay, hasScroll } = usePlay();
+  return (
+    <div
+      className={`overlay ${play ? "overlay--disable" : ""}
+    ${hasScroll ? "overlay--scrolled" : ""}`}
+    >
+      <div
+        className={`loader ${progress === 100 ? "loader--disappear" : ""}`}
+      />
+      {progress === 100 && (
+        <div className={`intro ${play ? "intro--disappear" : ""}`}>
+          <h4 className="logo">
+            Rekan- Rekan Sekalian
+          </h4>
+          <p className="intro__scroll">Scroll to begin the journey</p>
+          <button
+            className="explore"
+            onClick={() => {
+              setPlay(true);
+            }}
+          >
+            Lanjut Baca
+          </button>
+        </div>
+      )}
+      <div className={`outro ${end ? "outro--appear" : ""}`}>
+        <p className="outro__text">Wish you had a great flight with us...</p>
+      </div>
+    </div>
+  );
+};
